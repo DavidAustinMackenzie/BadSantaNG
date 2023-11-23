9a39f09dd50c5b9c2c284bc67e9a189eede5f2e5
David Mackenzie <dmackenzie1985@gmail.com>
2023-11-23 22:44:53 +1100

    Created home form and draw table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FontAwesomeModule,
+    FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
