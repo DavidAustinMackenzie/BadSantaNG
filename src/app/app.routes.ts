8429a250f8bea9de80fc36f40e9134e0810cb0ee
David Mackenzie <dmackenzie1985@gmail.com>
2023-11-23 22:00:30 +1100

    added draw component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DrawComponent } from './draw/draw.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: '', component: HomeComponent},
+    {path: 'draw', component: DrawComponent}
 ];
