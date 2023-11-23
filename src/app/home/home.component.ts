e137251e5b49219193b40d7ea1d66875bda8b0d5
David Mackenzie <dmackenzie1985@gmail.com>
2023-11-23 23:14:42 +1100

    added styling for place number

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,5 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  random = '?';
+  clicked = false;
 
+  onGenerateNumber() {
+    this.random = Math.floor((Math.random() * 10) + 1).toString();
+  }
+
+  onMousedown() {
+    this.clicked = true;
+  }
+
+  onMouseup() {
+    this.clicked = false;
+  }  
 }
